20b9b363edbe464ced988463159be5690d1b8993
hiimlex <alex.adaumi@gmail.com>
2022-09-04 14:21:16 -0300

    V: 0.0.2 - adding css

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,8 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
+import postcss from "rollup-plugin-postcss";
+
 const packageJson = require("./package.json");
 
 export default [
@@ -24,11 +26,13 @@ export default [
 			resolve(),
 			commonjs(),
 			typescript({ tsconfig: "./tsconfig.json" }),
+			postcss(),
 		],
 	},
 	{
 		input: "dist/esm/index.d.ts",
 		output: [{ file: "dist/index.d.ts", format: "esm" }],
 		plugins: [dts()],
+		external: [/\.css$/],
 	},
 ];
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Button.css";
 
 export interface ButtonProps {
 	label: string;
