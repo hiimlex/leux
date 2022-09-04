62534e01f6071a6e6a1f95b86e283eb0c4c7c521
hiimlex <alex.adaumi@gmail.com>
2022-09-04 16:22:54 -0300

    V: 0.0.4 - fix scss configuration

--- rollup.config.js
+++ rollup.config.js
@@ -37,6 +37,6 @@ export default [
 		input: "dist/esm/index.d.ts",
 		output: [{ file: "dist/index.d.ts", format: "esm" }],
 		plugins: [dts()],
-		external: [/\.css$/],
+		external: [/\.scss$/],
 	},
 ];
