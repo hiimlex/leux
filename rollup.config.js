84baa44f173d99c91a08532b6a49f109d8f0e2b7
hiimlex <alex.adaumi@gmail.com>
2022-09-04 14:37:57 -0300

    V: 0.0.4 - peer dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,10 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
-
 import postcss from "rollup-plugin-postcss";
+
+import { terser } from "rollup-plugin-terser";
+import PeerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const packageJson = require("./package.json");
 
@@ -27,6 +29,7 @@ export default [
 			commonjs(),
 			typescript({ tsconfig: "./tsconfig.json" }),
 			postcss(),
+			terser(),
 		],
 	},
 	{
