e727f5546da1b7df921838dba38fea42817666ca
Alex Rodrigues <alex.adaumi@gmail.com>
2022-09-05 20:25:23 -0300

    docs: project inittial structure

---
+++ docs/src/App.tsx
@@ -0,0 +1,12 @@
+import { LeButton, LeGlobalStyle } from "@hiimlex/leux";
+
+function App() {
+	return (
+		<div>
+			<LeGlobalStyle />
+			<LeButton label="Teste" />
+		</div>
+	);
+}
+
+export default App;
