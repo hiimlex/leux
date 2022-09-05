2e78ea84854c38e64ac45e9f37421953dbc88786
Alex Rodrigues <alex.adaumi@gmail.com>
2022-09-05 19:03:35 -0300

    V:0.1.10 - fix on global style component to return a div

--- src/components/LeGlobalStyle/LeGlobalStyle.tsx
+++ src/components/LeGlobalStyle/LeGlobalStyle.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./LeGlobalStyle.scss";
 
-const LeGlobalStyle = () => {};
+const LeGlobalStyle = () => (
+	<div className="le-styles" style={{ display: "none" }}></div>
+);
 
 export default LeGlobalStyle;
