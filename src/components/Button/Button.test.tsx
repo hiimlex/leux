6fcfee4bd021c4d8a09f26670a9ceaa5be8019eb
hiimlex <alex.adaumi@gmail.com>
2022-09-18 22:38:23 -0300

    test: improving button test's

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,10 +1,24 @@
 import { render } from "@testing-library/react";
 import React from "react";
-
 import { Button } from "./Button";
 
+import "@testing-library/jest-dom";
+
 describe("Button test", () => {
-	test("renders Button component", () => {
-		render(<Button>test</Button>);
+	it("should render Button component", () => {
+		const { container } = render(<Button label="Button test" />);
+		expect(container.firstChild).toHaveClass("le-button--default");
+	});
+
+	it("should render a Outlined Secondary Button component", () => {
+		const { container } = render(
+			<Button variant="secondary" type="outlined" size="large">
+				Secondary Button
+			</Button>
+		);
+
+		expect(container.firstChild).toHaveClass(
+			"le-button--outlined le-button--secondary le-button--large"
+		);
 	});
 });
