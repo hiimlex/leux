56ae44fc63869426189001ab2c210f218566b142
Alex Rodrigues <alex.rodrigues@venturus.org.br>
2023-05-07 12:36:02 -0300

    fix: adding code highlighter

--- docs/src/pages/Skeleton/SkeletonAttributes.tsx
+++ docs/src/pages/Skeleton/SkeletonAttributes.tsx
@@ -54,6 +54,58 @@ const SkeletonVariantPreview = () => {
 					</Skeleton>
 				</Box>
 			</LePreview>
+			{showCode && (
+				<LeHighlighter
+					code={`const Component = () => {
+	const [show, setShow] = useState(false);
+
+	const handleShow = () => {
+		setShow(!show);
+	};
+
+	return (
+		<Box flex flexDirection="column">
+			<Box flex flexDirection="row">
+				<Button
+					onClick={handleShow}
+					customStyles={{ marginRight: 12 }}
+					theme="primary"
+					variant={show ? "filled" : "outlined"}
+				>
+					{show ? "show" : "hide"}
+				</Button>
+			</Box>
+			<Box
+				flex
+				flexDirection="row"
+				alignItems="center"
+				justifyContent="space-between"
+				width="100%"
+			>
+				<Skeleton show={show} variant="paragraph" customStyles={{ marginRight: 12 }}>
+					<Topography
+						variant="body-1"
+						customStyles={{
+							marginRight: 12,
+							width: "30%",
+						}}
+					>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					</Topography>
+				</Skeleton>
+				<Skeleton show={show} variant="round" customStyles={{ marginRight: 12 }}>
+					<Avatar src="AL" asText></Avatar>
+				</Skeleton>
+				<Skeleton show={show} variant="square">
+					<Box flex bgColor="success" customStyles={{ width: 64, height: 64 }}></Box>
+				</Skeleton>
+			</Box>
+		</Box>
+	);		
+};`}
+					language="tsx"
+				/>
+			)}
 		</>
 	);
 };
@@ -114,6 +166,67 @@ const SkeletonSizePreview = () => {
 					</Skeleton>
 				</Box>
 			</LePreview>
+			{showCode && (
+				<LeHighlighter
+					code={`const Component = () => {
+	const [size, setSize] = useState<SkeletonSize>("medium");
+
+	const handleSize = (size: SkeletonSize) => {
+		setSize(size);
+	};
+
+	return (
+		<Box flex flexDirection="column">
+			<Box flex flexDirection="row">
+				<Button
+					onClick={() => handleSize("small")}
+					customStyles={{ marginRight: 12 }}
+					theme="primary"
+					variant={size === "small" ? "filled" : "outlined"}
+				>
+					small
+				</Button>
+				<Button
+					onClick={() => handleSize("medium")}
+					customStyles={{ marginRight: 12 }}
+					theme="primary"
+					variant={size === "medium" ? "filled" : "outlined"}
+				>
+					medium
+				</Button>
+				<Button
+					onClick={() => handleSize("large")}
+					customStyles={{ marginRight: 12 }}
+					theme="primary"
+					variant={size === "large" ? "filled" : "outlined"}
+				>
+					large
+				</Button>
+			</Box>
+			<Box
+				flex
+				flexDirection="row"
+				alignItems="center"
+				justifyContent="space-between"
+				width="100%"
+			>
+				<Skeleton active show size={size} width="100%">
+					<Topography
+						variant="body-1"
+						customStyles={{
+							marginRight: 12,
+						}}
+					>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					</Topography>
+				</Skeleton>
+			</Box>
+		</Box>
+	);		
+};`}
+					language="tsx"
+				/>
+			)}
 		</>
 	);
 };
@@ -149,13 +262,56 @@ const SkeletonRowsPreview = () => {
 					justifyContent="space-between"
 					width="100%"
 				>
-					<Skeleton active show rows={rows} variant="paragraph">
+					<Skeleton show rows={rows} variant="paragraph">
 						<Topography variant="body-1">
 							Lorem ipsum dolor sit amet consectetur adipisicing elit.
 						</Topography>
 					</Skeleton>
 				</Box>
 			</LePreview>
+			{showCode && (
+				<LeHighlighter
+					language="tsx"
+					code={`const Component = () => {
+	const [rows, setRows] = useState(2);
+
+	const handleRows = (rows: number) => {
+		setRows((curr) => curr + rows);
+	};
+
+	return (
+		<Box flex flexDirection="column">
+			<Box flex flexDirection="row">
+				<Button
+					state={{ disabled: rows === 5 }}
+					onClick={() => handleRows(+1)}
+					customStyles={{ marginRight: 12 }}
+					theme="primary"
+				>
+					+
+				</Button>
+				<Button state={{ disabled: rows === 1 }} onClick={() => handleRows(-1)} theme="danger">
+					-
+				</Button>
+			</Box>
+			<Box
+				flex
+				flexDirection="row"
+				alignItems="center"
+				justifyContent="space-between"
+				width="100%"
+			>
+				<Skeleton active show rows={rows} variant="paragraph">
+					<Topography variant="body-1">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					</Topography>
+				</Skeleton>
+			</Box>
+		</Box>
+	)
+};`}
+				/>
+			)}
 		</>
 	);
 };
@@ -190,6 +346,41 @@ const SkeletonActivePreview = () => {
 					</Skeleton>
 				</Box>
 			</LePreview>
+			{showCode && (
+				<LeHighlighter
+					language="tsx"
+					code={`const Component = () => {
+	const [active, setActive] = useState(false);
+
+	const handleActive = () => {
+		setActive((curr) => !curr);
+	};
+					
+	return (
+		<Box flex flexDirection="column">
+			<Box flex flexDirection="row">
+				<Button onClick={handleActive} theme="primary" variant={active ? "filled" : "outlined"}>
+					{active ? "active" : "inactive"}
+				</Button>
+			</Box>
+			<Box
+				flex
+				flexDirection="row"
+				alignItems="center"
+				justifyContent="space-between"
+				width="100%"
+			>
+				<Skeleton active={active} show variant="paragraph">
+					<Topography variant="body-1">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit.
+					</Topography>
+				</Skeleton>
+			</Box>
+		</Box>
+	)
+};`}
+				/>
+			)}
 		</>
 	);
 };
