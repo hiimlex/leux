82a49cb35353f48cee7c617a5d7ee1be6cfebc63
Alex Rodrigues <alex.rodrigues@venturus.org.br>
2024-02-24 21:18:45 -0300

    ci/cd: update rollup package

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 
-import { terser } from "rollup-plugin-terser";
+import { terser } from "@rollup/plugin-terser";
 
 import styles from "rollup-plugin-styles";
 
