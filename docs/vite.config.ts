882f0178fe23f7a6f997ea72b5c3f8c40bcc07d4
Alex Rodrigues <alex.adaumi@gmail.com>
2025-06-24 09:20:15 -0300

    trying new config

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -7,30 +7,36 @@ import markdownIt from "markdown-it";
 
 import markdownItAnchor from "markdown-it-anchor";
 
-export default defineConfig(async () => ({
-	plugins: [
-		react(),
-		(mdPlugin as any).default({
-			mode: [Mode.REACT],
-			markdownIt: markdownIt({ html: true }).use(markdownItAnchor, {
-				tabIndex: false,
+export default defineConfig(async ({ command }) => {
+	const isDev = command === "serve";
+
+	return {
+		plugins: [
+			react(),
+			(mdPlugin as any).default({
+				mode: [Mode.REACT],
+				markdownIt: markdownIt({ html: true }).use(markdownItAnchor, {
+					tabIndex: false,
+				}),
 			}),
-		}),
-	],
-	resolve: {
-		alias: [
-			{ find: "@", replacement: resolve(__dirname, "src") },
-			{
-				find: "@leux/ui",
-				replacement: resolve(__dirname, "../src"),
-			},
 		],
-	},
-	server: {
-		port: 5000,
-		fs: {
-			allow: [".."],
+		resolve: {
+			alias: [
+				{ find: "@", replacement: resolve(__dirname, "src") },
+				isDev
+					? {
+							find: "@leux/ui",
+							replacement: resolve(__dirname, "../src"),
+					  }
+					: {},
+			],
 		},
-	},
-	assetsInclude: ["**/*.svg"],
-}));
+		server: {
+			port: 5000,
+			fs: {
+				allow: [".."],
+			},
+		},
+		assetsInclude: ["**/*.svg"],
+	};
+});
